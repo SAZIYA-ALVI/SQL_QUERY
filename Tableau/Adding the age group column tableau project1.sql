0c30de680b8d11230dae5d9e2d975b95812d066c
Saziya Alvi <saziyaalvi7@gmail.com>
2026-09-04 21:14:03 +0530

    Add files via upload

---
+++ Tableau/Adding the age group column tableau project1.sql
@@ -0,0 +1,20 @@
+-- Database - Tableau project 1
+-- Adding the age group column
+
+--vid 3
+select age, count(*) [count] from [dbo].[Depression+Student+Dataset]
+group by age
+order by age desc
+
+alter table [dbo].[Depression+Student+Dataset]
+add Age_Group varchar(max)
+select * from [dbo].[Depression+Student+Dataset]
+
+update [dbo].[Depression+Student+Dataset]
+set Age_Group = 
+case when Age between 18 and 24 then 'A1' 
+ELSE case when Age between 25 and 30 then 'A2'
+else 'A3' end end
+
+SELECT  age_group , count(*) from [dbo].[Depression+Student+Dataset]
+group by age_group
